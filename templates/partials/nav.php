70f78b4f28662eb13c7c7ee9aac5622829e9dcb1
AwaisSherwani97 <75368467+AwaisSherwani97@users.noreply.github.com>
2021-05-28 09:54:46 +0100

    Update nav.php

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -5,6 +5,7 @@
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="students.php">Students List</a> |
+   <a href="addstudent.php">Add Students </a> |
    <a href="logout.php">Logout</a>
    
 
